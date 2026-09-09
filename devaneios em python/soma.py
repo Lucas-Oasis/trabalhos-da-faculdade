b9caa562a957a21c7bca4b0c44bc5758fbd54551
Lucas-Oasis <lucas.oasis.dev@gmail.com>
2026-09-09 10:54:45 -0300

    Add files via upload
    simplicidades em python

---
+++ devaneios em python/soma.py
@@ -0,0 +1,5 @@
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+resultado = (n1 + n2)
+#print('A soma entre', n1, ' e ', n2,' vale',resultado)
+print('A soma entre {} e {} vale {}'.format(n1,n2,resultado))
