485b144e44c10da35028c0f0a5720ade5083b57f
Lucas Oásis <lucas.oasis.dev@gmail.com>
2026-09-16 15:43:10 -0300

    Add files via upload

---
+++ desafio-alexandre/desafio.py
@@ -0,0 +1,50 @@
+sim = 0
+não = 0
+
+Sim = 0
+Não = 0
+
+s = 0
+n = 0
+
+SIM = 0
+NAO = 0
+
+print('Um homem foi morto na última madrugada')
+print('A policia começa a fazer as investigações')
+print('Você será interrogado...')
+
+pergunta_1 = input('Telefonou para a vítima? ').lower().replace('.','')
+if pergunta_1 == 'sim':
+    sim += 1
+else:
+    não += 1
+pergunta_2 = input('Esteve no local do crime? ').lower().replace('.','')
+if pergunta_2 == 'sim':
+    sim += 1
+else:
+    não += 1
+pergunta_3 = input('Mora perto da vítima? ').lower().replace('.','')
+if pergunta_3 == 'sim':
+    sim += 1
+else:
+    não += 1
+pergunta_4 = input('Devia para a vítima? ').lower().replace('.','')
+if pergunta_4 == 'sim':
+    sim += 1
+else:
+    não += 1
+pergunta_5 = input('Já trabalhou com a vítima? ').lower().replace('.','')
+if pergunta_5 == 'sim':
+    sim += 1
+else:
+    não += 1
+
+if sim >= 2 and sim < 3:
+    print('Você é suspeito.')
+elif sim >= 3 and sim <= 4:
+    print('Você é cúmplice.')
+elif sim == 5:
+    print('Você é o assassino.')
+else:
+    print('Você é inocente')
